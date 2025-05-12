da8107e81052ab41ca22c5d4780309dc116f7d89
joelmandell2 <jmandel2@seas.upenn.edu>
2025-05-12 17:39:08 -0400

    Background video complete

--- src/pages/SecurityPage.js
+++ src/pages/SecurityPage.js
@@ -2,6 +2,7 @@ import { useEffect, useState ,} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Container,Card, CardContent, TextField, Button} from '@mui/material';
 
+import vid from './videos/retriever.mp4';
 
 
 
@@ -52,7 +53,23 @@ export default function SecurityPage(){
 
     return(
         <Container sx={{align:'center', justifyContent: 'center', display:'flex'}}>
-            <Card sx={{align:'center', justifyContent: 'center', mt: 30,  width:'300px'}}>
+            <video
+                autoPlay
+                muted
+                playsInline
+                loop
+                id="ny_vid"
+                style={{
+                    position: 'absolute',
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'cover',
+                }}
+            >
+                <source src={vid} type="video/mp4" />
+            </video>
+            <Card sx={{align:'center', justifyContent: 'center', mt: 30,  width:'300px', backdropFilter: 'blur(10px)',
+                    backgroundColor: 'rgba(255, 255, 255, 0.4)',}}>
                 <CardContent sx={{display: 'flex', flexDirection: 'column',align: 'center', justifyContent: 'center'}}>
                     <TextField onChange={handleUser} id="outlined-basic" label="Name" variant="outlined" required/>
                     <TextField onChange={handlePassword} id="pass" label="Email" variant="outlined" required sx={{mt:2}}/>
